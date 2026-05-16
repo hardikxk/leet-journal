929e6ddc63da82830dbb5a1a5f91665f006c1950
Saptarshi Saha <saptarshiok26@gmail.com>
2026-05-16 13:18:59 +0530

    Create GlobalExceptionHandler.java

---
+++ problems-service/src/main/java/com/hardik/problemsservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.hardik.problemsservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(
+            IllegalArgumentException ex) {
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ex.getMessage());
+    }
+}
