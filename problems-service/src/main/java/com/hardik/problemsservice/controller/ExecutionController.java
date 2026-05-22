a71b608a5928cfdd048f4deb56763941c0628a5f
Arshav Suman <arshavsuman@gmail.com>
2026-05-22 16:29:29 +0530

    Configure Swagger/OpenAPI for problems-service endpoints

--- problems-service/src/main/java/com/hardik/problemsservice/controller/ExecutionController.java
+++ problems-service/src/main/java/com/hardik/problemsservice/controller/ExecutionController.java
@@ -1,9 +1,12 @@
 package com.hardik.problemsservice.controller;
 
 import com.hardik.problemsservice.service.LocalExecutorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Execution API", description = "Operations related to code execution")
 @RestController
 public class ExecutionController {
 
@@ -13,6 +16,7 @@ public class ExecutionController {
         this.localExecutorService = localExecutorService;
     }
 
+    @Operation(summary = "Run Java code locally")
     @GetMapping("/run")
     public String run() {
         return localExecutorService.executeJava();
