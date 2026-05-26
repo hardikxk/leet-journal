d83af73b2db02e282cf957a77acee43c322d065c
Malhar-M <malharmore486@gmail.com>
2026-05-26 23:08:45 +0530

    feat: change java code execution from static to dynamic

--- problems-service/src/main/java/com/hardik/problemsservice/service/LocalExecutorService.java
+++ problems-service/src/main/java/com/hardik/problemsservice/service/LocalExecutorService.java
@@ -1,22 +1,18 @@
 package com.hardik.problemsservice.service;
 
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
-
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
 
 @Service
 public class LocalExecutorService {
 
-    public String executeJava(){
+    public String executeJava(String userCode){ // This will accept user's code input
         Path tempFile = null;
         try{
-            Resource classPathResource = new ClassPathResource("BinarySearch.java");
             tempFile = Files.createTempFile("run-", ".java");
-            Files.copy(classPathResource.getInputStream(), tempFile, StandardCopyOption.REPLACE_EXISTING);
+            // User's code is written directly to the temporary file
+            Files.writeString(tempFile, userCode);
 
             ProcessBuilder pb = new ProcessBuilder("java",  tempFile.toString());
             pb.redirectErrorStream(true);
