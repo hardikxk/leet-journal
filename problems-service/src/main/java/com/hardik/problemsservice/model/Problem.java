6f9fd803b275b090dbdce0a304acaa43c9445abd
Rohit Verma <rohitgcode20@gmail.com>
2026-05-30 17:44:47 +0530

    Add @Id annotation to Problem record id field

--- problems-service/src/main/java/com/hardik/problemsservice/model/Problem.java
+++ problems-service/src/main/java/com/hardik/problemsservice/model/Problem.java
@@ -1,3 +1,3 @@
 package com.hardik.problemsservice.model;
 
-public record Problem(int id, String title, String acceptance, String difficulty){}
+public record Problem( @Id int id, String title, String acceptance, String difficulty){}
